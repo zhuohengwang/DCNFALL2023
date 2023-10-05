bc6f78a8cd91817f414ec86ec00922767f625205
zhuohengwang <147107330+zhuohengwang@users.noreply.github.com>
2023-10-05 17:35:49 -0400

    Update run.py

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,11 +1,16 @@
 from flask import Flask
+import datetime
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
-
+@app.route('/time')
+def get_current_time():
+    current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return 'Current Time: {}'.format(current_time)
 
 app.run(host='0.0.0.0',
         port=8080,
